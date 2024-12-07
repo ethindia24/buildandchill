37f098aefe9831b40e29fa9d7df0b0881394d8d5
ss251 <ss251@uw.edu>
2024-12-07 12:10:33 +0530

    feat: add onchainkit connect wallet button

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import "./globals.css"
 import Header from "@/components/Header"
+import { Providers } from "./providers"
+import '@coinbase/onchainkit/styles.css';
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -18,10 +20,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+      <Providers>
         <div className="flex h-screen flex-col">
           <Header />
           <main className="flex-1 overflow-hidden">{children}</main>
         </div>
+        </Providers>
       </body>
     </html>
   )
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,4 +1,5 @@
 "use client"
+import { WalletDefault } from '@coinbase/onchainkit/wallet';
 
 export default function Header() {
 
@@ -7,7 +8,7 @@ export default function Header() {
       <h1 className="text-2xl font-bold">buildandchill</h1>
       
       <div className="flex items-center space-x-4">
-        
+      <WalletDefault />
       </div>
     </header>
   )
